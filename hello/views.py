0788a9437b02e2104b7fe63d63fd89967da95bb7
Frank <frankdai320@gmail.com>
2016-11-26 15:58:22 -0800

    add upload-csv (not really csv

--- hello/views.py
+++ hello/views.py
@@ -4,6 +4,9 @@ from django.http import HttpResponse
 from .models import Greeting
 from .models import Question
 
+import csv_parse
+
+
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
@@ -20,6 +23,12 @@ def generateset(request):
 def questionset(request):
     # questions = Question.objects.all()
     return render(request, 'questionset.html')
+
+def upload_csv(request):
+    if request.method == 'POST':
+        text = request.POST['upload']
+        csv_parse(text)
+    return render(request, 'index.html')
 
 def db(request):
 
