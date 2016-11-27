94ae881c515b488571280e904849cdcc78a11048
Frank <frankdai320@gmail.com>
2016-11-26 17:43:59 -0800

    make everything nullable

--- hello/models.py
+++ hello/models.py
@@ -5,20 +5,20 @@ class Greeting(models.Model):
     when = models.DateTimeField('date created', auto_now_add=True)
 
 class Question(models.Model):
-	comp = models.CharField(max_length=100)
-	subject = models.CharField(max_length=100)
-	subtopic = models.CharField(max_length=100)
-	category = models.CharField(max_length=100)
-	name = models.CharField(max_length=100)
-	set_number = models.CharField(max_length=4)
-	date_written = models.DateTimeField('date created', auto_now_add=True) #maybe not correct
-	date_last_mod = models.DateTimeField('date last modified', auto_now_add=True) #maybe not correct
-	question_type = models.CharField(max_length=20)
-	question_text = models.CharField(max_length=10000) # change max length?
-	short_answer_answer_text = models.CharField(max_length=10000)
-	w = models.CharField(max_length=100)
-	x = models.CharField(max_length=100)
-	y = models.CharField(max_length=100)
-	z = models.CharField(max_length=100)
-	correct = models.CharField(max_length=1)
-	difficulty = models.IntegerField()
+	comp = models.CharField(max_length=100,default=None, blank=True, null=True)
+	subject = models.CharField(max_length=100, default=None, blank=True, null=True)
+	subtopic = models.CharField(max_length=100, default=None, blank=True, null=True)
+	category = models.CharField(max_length=100, default=None, blank=True, null=True)
+	name = models.CharField(max_length=100, default=None, blank=True, null=True)
+	set_number = models.CharField(max_length=4, default=None, blank=True, null=True)
+	date_written = models.DateTimeField('date created', auto_now_add=True, default=None, blank=True, null=True) #maybe not correct
+	date_last_mod = models.DateTimeField('date last modified', auto_now_add=True, default=None, blank=True, null=True) #maybe not correct
+	question_type = models.CharField(max_length=20, default=None, blank=True, null=True)
+	question_text = models.CharField(max_length=10000, default=None, blank=True, null=True) # change max length?
+	short_answer_answer_text = models.CharField(max_length=10000, default=None, blank=True, null=True)
+	w = models.CharField(max_length=100, default=None, blank=True, null=True)
+	x = models.CharField(max_length=100, default=None, blank=True, null=True)
+	y = models.CharField(max_length=100, default=None, blank=True, null=True)
+	z = models.CharField(max_length=100, default=None, blank=True, null=True)
+	correct = models.CharField(max_length=1, default=None, blank=True, null=True)
+	difficulty = models.IntegerField(default=None, blank=True, null=True)
