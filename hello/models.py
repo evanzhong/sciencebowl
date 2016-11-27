76a0665bc92e4219ee4c82ad63505584079d337b
Andrew Lou <andy.lou98@gmail.com>
2016-11-26 16:11:59 -0800

    removed topics, authors classes, inserted into questions

--- hello/models.py
+++ hello/models.py
@@ -4,21 +4,15 @@ from django.db import models
 class Greeting(models.Model):
     when = models.DateTimeField('date created', auto_now_add=True)
 
-class Topics(models.Model):
+class Question(models.Model):
 	comp = models.CharField(max_length=100)
 	subject = models.CharField(max_length=100)
 	subtopic = models.CharField(max_length=100)
 	category = models.CharField(max_length=100)
-
-class Author(models.Model):
 	name = models.CharField(max_length=100)
 	set_number = models.CharField(max_length=4)
-
-class Question(models.Model):
-	subject = models.ForeignKey(Topics, on_delete=models.CASCADE)
-	author = models.ForeignKey(Author, on_delete=models.CASCADE)
 	date_written = models.DateTimeField('date created', auto_now_add=True) #maybe not correct
-	date_last_mod = models.DateTimeField('date created', auto_now_add=True) #maybe not correct
+	date_last_mod = models.DateTimeField('date last modified', auto_now_add=True) #maybe not correct
 	question_type = models.CharField(max_length=20)
 	question_text = models.CharField(max_length=10000) # change max length?
 	short_answer_answer_text = models.CharField(max_length=10000)
@@ -28,4 +22,3 @@ class Question(models.Model):
 	z = models.CharField(max_length=100)
 	correct = models.CharField(max_length=1)
 	difficulty = models.IntegerField()
-	
