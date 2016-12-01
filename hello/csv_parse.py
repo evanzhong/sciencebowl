d84619ced458799fdc5344f7a84380f5e640ef30
Evan <evanzhong10@gmail.com>
2016-12-01 11:56:17 -0800

    Modified for tsv format (output of parser)

--- hello/csv_parse.py
+++ hello/csv_parse.py
@@ -3,18 +3,25 @@ from hello.models import Question
 
 def parse(text):
     rows = text.split("\n")
-    types = rows[0].split(",");
+    types = rows[0].split("\t");
 
     for row in rows:
-        obj = row.split(",")
+        obj = row.split("\t")
         question = Question()
-        if len(obj) > 2:
-            question.question_text = obj[3];
-            question.w = obj[4];
-            question.x = obj[5];
-            question.y = obj[6];
-            question.z = obj[7];
-            question.correct = obj[8];
+        if len(obj) = 13:
+            question.comp = obj[0];
+            question.subject = obj[1];
+            question.subtopic = obj[2];
+            question.question_type = obj[3];
+            question.question_text = obj[4];
+            question.w = obj[5];
+            question.x = obj[6];
+            question.y = obj[7];
+            question.z = obj[8];
+            question.correct = obj[9];
+            question.short_answer_answer_text = obj[10];
+            question.name = obj[11];
+            question.set_number = obj[12];
             question.save()
         else:
             print("failed to write question (length is not right)")
