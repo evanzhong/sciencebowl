ae87db7b5f1c2263e853aedea84105f560651d30
Evan <evanzhong10@gmail.com>
2016-12-01 23:27:45 -0800

    testing ~ separated values

--- hello/parse.py
+++ hello/parse.py
@@ -5,7 +5,7 @@ def parse(text):
     types = rows[0].split("\t");
 
     for row in rows:
-        obj = row.split("\t")
+        obj = row.split("~")
         question = Question()
         if len(obj) == 13:
             question.comp = obj[0];
