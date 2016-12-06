3636defdf095f0a68a4a1a39fe24306001135e79
Evan <evanzhong10@gmail.com>
2016-12-06 12:08:26 -0800

    Writing the query

--- hello/views.py
+++ hello/views.py
@@ -41,8 +41,7 @@ def generateset(request):
         scrammbleQs = request.POST.get('scrammbleQs')
         if scrammbleQs == "":
             raise RuntimeError("empty values")
-        questions = Question.objects.filter(comp__iexact=comp)#.filter(subject__iexact=subs[0])
-        questions.order_by('?')[:numQs]
+        questions = Question.objects.filter(comp__iexact=comp).order_by('?')[:numQs]#.filter(subject__iexact=subs[0])
         return render(request, 'questionset.html', {'questions': questions})
     return render(request, 'generateset.html')
 
