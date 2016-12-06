1129c9c266cbf6c5e1ec57429d349312a10caa80
Evan <evanzhong10@gmail.com>
2016-12-05 18:00:50 -0800

    Changed Max-length to 500 for MC choices

--- hello/models.py
+++ hello/models.py
@@ -10,11 +10,11 @@ class Question(models.Model):
 	subtopic = models.CharField(max_length=100, default=None, blank=True, null=True) #This will be harder to keep consistant -> will need better system
 	question_type = models.CharField(max_length=20, default=None, blank=True, null=True)
 	question_text = models.CharField(max_length=10000, default=None, blank=True, null=True) # change max length?
-	w = models.CharField(max_length=100, default=None, blank=True, null=True)
-	x = models.CharField(max_length=100, default=None, blank=True, null=True)
-	y = models.CharField(max_length=100, default=None, blank=True, null=True)
-	z = models.CharField(max_length=100, default=None, blank=True, null=True)
-	correct = models.CharField(max_length=100, default=None, blank=True, null=True)
+	w = models.CharField(max_length=500, default=None, blank=True, null=True)
+	x = models.CharField(max_length=500, default=None, blank=True, null=True)
+	y = models.CharField(max_length=500, default=None, blank=True, null=True)
+	z = models.CharField(max_length=500, default=None, blank=True, null=True)
+	correct = models.CharField(max_length=500, default=None, blank=True, null=True)
 	short_answer_answer_text = models.CharField(max_length=10000, default=None, blank=True, null=True)
 	name = models.CharField(max_length=100, default=None, blank=True, null=True) #Author (First Last)
 	set_number = models.CharField(max_length=4, default=None, blank=True, null=True) 
