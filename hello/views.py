8e30b710d6ad448edb181c1b61dcfd8ef8fd6f87
Evan <evanzhong10@gmail.com>
2016-12-06 18:20:27 -0800

    Debugging

--- hello/views.py
+++ hello/views.py
@@ -50,8 +50,7 @@ def generateset(request):
             return render(request, 'questionset.html', {'questions': questions})
         elif TUAB:
             tt = int(numQs) * 2
-            counter = 0
-            questions = Question.objects.filter(comp__iexact=comp).filter(subject__in=subs).order_by('?')[:tt]
+            questions = Question.objects.filter(comp__iexact=comp).filter(subject__in=subs)[:tt]
             return render(request, 'questionset.html', {'questions': questions, 'includeBonuses': TUAB})
     return render(request, 'generateset.html')
 
