5473ce0e1bac5bee35f4f508c34191e4199c8747
Evan <evanzhong10@gmail.com>
2016-12-07 10:11:39 -0800

    Added Testing user

--- hello/views.py
+++ hello/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 from .models import Greeting
 from .models import Question
