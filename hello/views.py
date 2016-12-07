8e7bff5c3bc066f54766d0bb0a0f75198238cfab
Evan <evanzhong10@gmail.com>
2016-12-07 10:22:57 -0800

    Added Testing user

--- hello/views.py
+++ hello/views.py
@@ -8,15 +8,6 @@ from .models import Question
 
 import parse
 
-# Creating user to test
-from django.contrib.auth.models import User
-user = User.objects.create_user(
-    username = 'ScienceBowl',
-    password = 'cowseatcereal888',
-    email = 'evanzhong10@gmail.com'
-  )
-user.save()
-
 # Create your views here.
 @login_required
 def index(request):
