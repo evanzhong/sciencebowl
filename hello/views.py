eee93655072a82b1d6facaec2f0538c8d44beec3
Evan <evanzhong10@gmail.com>
2016-12-06 21:20:30 -0800

    Added @login_required

--- hello/views.py
+++ hello/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.decorators import login_required
 
 from .models import Greeting
 from .models import Question
@@ -8,18 +9,21 @@ from .models import Question
 import parse
 
 # Create your views here.
+@login_required
 def index(request):
     return render(request, 'index.html')
 
 def login(request):
     return render(request, 'registration/login.html')
 
+@login_required
 def addquestions(request):
     return render(request, 'addquestions.html')
-
+@login_required
 def questionsconfirmed(request):
     return render(request, 'questionsconfirmed.html')
 
+@login_required
 @csrf_exempt
 def generateset(request):
     if request.method == 'POST':
@@ -50,14 +54,16 @@ def generateset(request):
             return render(request, 'questionset.html', {'questions': questions})
         elif TUAB:
             tt = int(numQs) * 2
-            questions = Question.objects.filter(comp__iexact=comp).filter(subject__in=subs).order_by('subject')[:tt]
+            questions = Question.objects.filter(comp__iexact=comp).filter(subject__in=subs).order_by('?')[:tt]
             return render(request, 'questionset.html', {'questions': questions, 'includeBonuses': TUAB})
     return render(request, 'generateset.html')
 
+@login_required
 def questionset(request):
     questions = Question.objects.all()
     return render(request, 'questionset.html', {'questions': questions})
 
+@login_required
 @csrf_exempt
 def upload(request):
     if request.method == 'POST':
@@ -67,6 +73,7 @@ def upload(request):
         parse.parse(text)
     return render(request, 'index.html')
 
+@login_required
 def db(request):
 
     greeting = Greeting()
