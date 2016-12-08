d5b1c7e7c853ac029a8770b510a1f9604c71428f
Evan <evanzhong10@gmail.com>
2016-12-08 15:53:57 -0800

    Debugging

--- hello/views.py
+++ hello/views.py
@@ -76,8 +76,8 @@ def generateset(request):
             # Generation of Bonuses
             bonusQuestions = []
             for subjectOfEach in subsOfQuestions:
-                bq = Question.objects.filter(comp__iexact=comp).filter(subject__in=subjectOfEach).order_by('?')[0]
-                # bq = Question.objects.get(comp__iexact=comp, subject__iexact=subjectOfEach)#.order_by('?')
+                # bq = Question.objects.filter(comp__iexact=comp).filter(subject__in=subjectOfEach).order_by('?')[0]
+                bq = Question.objects.get(comp__iexact=comp, subject__iexact=subjectOfEach)#.order_by('?')
                 bonusQuestions.append(bq)
             return render(request, 'questionset.html', {'questions': questions, 'bonusQuestions': bonusQuestions, 'includeBonuses': TUAB})
     return render(request, 'generateset.html')
