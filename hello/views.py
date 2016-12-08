5a99fc577297e6f923f0f47e45908340e7df469d
Evan <evanzhong10@gmail.com>
2016-12-08 11:57:32 -0800

    Adding looping through dictionary

--- hello/views.py
+++ hello/views.py
@@ -76,9 +76,9 @@ def generateset(request):
                 i += 1
 
             # Generation of Bonuses
-            bonusQuestions = {}
-            for key, value in subsOfQuestions.iteritems():
-                bonusQuestions[key] = Question.objects.filter(comp__iexact=comp).filter(subject__in=key).order_by('?')[:value]
+            bonusQuestions = []
+            for key, value in subsOfQuestions.items():
+                bonusQuestions[key] = Question.objects.filter(comp__iexact=comp).filter(subject__in=value).order_by('?')[:1]
             print bonusQuestions
             return render(request, 'questionset.html', {'questions': questions, 'bonusQuestions': bonusQuestions, 'includeBonuses': TUAB})
     return render(request, 'generateset.html')
