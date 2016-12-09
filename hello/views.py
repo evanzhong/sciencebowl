0e52e6284d4c8578285ba8348598f49e1e5ac935
Evan <evanzhong10@gmail.com>
2016-12-08 17:08:25 -0800

    Added zipList formatting

--- hello/views.py
+++ hello/views.py
@@ -79,7 +79,7 @@ def generateset(request):
                 bonusQuestions.append(Question.objects.filter(comp__iexact=comp).filter(subject__in=subs).order_by('?')[:1])
             flattened = [val for sublist in bonusQuestions for val in sublist]
             zList = zip(questions, flattened)
-            return render(request, 'questionset.html', {'zList': zList, 'questions': questions, 'bonusQuestions': flattened, 'includeBonuses': TUAB})
+            return render(request, 'questionset.html', {'zList': zList, 'includeBonuses': TUAB})
     return render(request, 'generateset.html')
 
 @login_required
