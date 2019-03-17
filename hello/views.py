4526cc482ce012b7e7d5d8b54f842e265f6f0d6d
evazhog <evanzhong10@gmail.com>
2019-03-16 22:40:10 -0700

    testing

--- hello/views.py
+++ hello/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.decorators import login_required
+import itertools
 
 from .models import Greeting
 from .models import Question
