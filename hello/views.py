be7e8016f5e1a69458d0104e411486c109c7d175
evazhog <evanzhong10@gmail.com>
2019-03-16 22:17:03 -0700

    testing

--- hello/views.py
+++ hello/views.py
@@ -70,7 +70,8 @@ def generateset(request):
                 for each in zip(nosbSubs, subs):
                     percentage = round(each[1] * int(numQs) * 0.01)
                     subject = each[0]
-                    questions = Question.objects.filter(comp__iexact="NOSB").filter(subject__iexact=subject).order_by('?')[:percentage]
+                    temp = Question.objects.filter(comp__iexact="NOSB").filter(subject__iexact=subject).order_by('?')[:percentage]
+                    questions,append(temp)
                 return render(request, 'questionset.html', {'questions': questions, 'includeBonuses': TUAB})
             else:
                 questions = Question.objects.filter(comp__iexact=comp).filter(subject__in=subs).order_by('?')[:numQs]
